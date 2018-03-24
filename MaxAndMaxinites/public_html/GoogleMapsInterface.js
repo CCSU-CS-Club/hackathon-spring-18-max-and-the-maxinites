4394272208785977209c990d580af4099408fd90
James Luczynski <j.luczynski@my.ccsu.edu>
2018-03-24 13:50:46 -0400

    Add some basic functions for interacting with google maps

--- MaxAndMaxinites/public_html/GoogleMapsInterface.js
+++ MaxAndMaxinites/public_html/GoogleMapsInterface.js
@@ -6,7 +6,52 @@ var map;
         center: {lat: 41.69, lng: -72.76},
         zoom: 8
       });
-    }
-
-
-
+    };
+    
+    
+    /**
+     * Returns raw text of the provided url
+     * @param {type} url
+     * @returns {Array|Object}
+     */
+    requestRawText : function(url){ 
+        var req = new XMLHttpRequest();
+        req.open('GET', url, false);
+        req.send(null);   
+        return req.responseText;        
+    };
+    
+    /**
+     * Computes distance in miles between the lat, lng positions provided
+     * @param {type} pos1 (lat, lng) of first position
+     * @param {type} pos2 (lat, lng) of second position
+     * @returns {Number} distance between the two positions in miles
+     */
+    distance: function(pos1, pos2){
+    //returns distance in miles.
+        pos1 = new google.maps.LatLng(JSON.parse(JSON.stringify(pos1)));
+        pos2 = new google.maps.LatLng(JSON.parse(JSON.stringify(pos2)));
+        var dist = google.maps.geometry.spherical.computeDistanceBetween(pos1, pos2); //in meters
+        dist = dist / 1609.34; //convert to miles
+        return dist;       
+    };
+            
+        /**
+     * Takes user address as a string, makes a call to google's geocoding service
+     * returns the JSON object of the resulting call to google's geocoding service
+     * which contains the latitude, longitude of the user address, among many 
+     * other things.
+     * @param {String} address
+     * @returns {Array|Object}
+     */
+    geocode: function(address){
+        address = address.replace(" ", "+");
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' 
+            + address + '&components=state:CT&key=AIzaSyBezkqLyMpXAF9dBb4X5rZeQkyF8Y5_Te4';  
+        return JSON.parse(request(url));
+    };
+            
+    getTextInput: function(){
+        var text = document.input_form.text.value;
+        return text;
+    };
