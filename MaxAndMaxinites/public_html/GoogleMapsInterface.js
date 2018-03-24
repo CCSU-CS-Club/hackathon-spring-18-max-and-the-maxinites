89c200b5de608f2351600255ae5ff1d24d014702
James Luczynski <j.luczynski@my.ccsu.edu>
2018-03-24 13:39:54 -0400

    Move JavaScript to separte javascript file

--- MaxAndMaxinites/public_html/GoogleMapsInterface.js
+++ MaxAndMaxinites/public_html/GoogleMapsInterface.js
@@ -1,5 +1,12 @@
 // GoogleMapsAPI Key: AIzaSyDK8uA17TfC1LEJ3OIoAYo2CP9VsJXBkSI
 
+var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.69, lng: -72.76},
+        zoom: 8
+      });
+    }
 
 
 
