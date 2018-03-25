943996abe7e288e56d54f86192ade40a133f6aaa
Austin Barrett <austinbrrtt@gmail.com>
2018-03-24 21:59:25 -0400

    Added transitions to menu

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import fontAwesome from '@fortawesome/fontawesome'
 import {faBars} from '@fortawesome/fontawesome-free-solid'
-import MapCont from './Map'
+import Map from './Map'
 import googleMaps from '@google/maps'
 
 fontAwesome.library.add(faBars)
@@ -12,6 +12,9 @@ class App extends React.Component{
 		super(props)
 		this.toggleMenu = this.toggleMenu.bind(this)
 		this.state = {
+			googleMapsClient: googleMaps.createClient({
+				key: 'AIzaSyDK8uA17TfC1LEJ3OIoAYo2CP9VsJXBkSI'
+			}),
 			hideMenu: true
 		}
 	}
@@ -19,16 +22,14 @@ class App extends React.Component{
 	render(){
 		return(
 			<>
-			<div 
-				className={'menu-modal' + (this.state.hideMenu ? ' menu-hidden' : '') } 
-				onClick={this.toggleMenu}>
-				<div className="menu">{"I'm a menu"}</div>
+			<div className={'menu-modal' + (this.state.hideMenu ? ' menu-modal-hidden' : '') } onClick={this.toggleMenu}>
+				<div className={'menu' + (this.state.hideMenu ? ' menu-hidden' : '') }>{"I'm a menu"}</div>
 			</div>
 			<div className="top-bar">
 				<div className="menu-toggle" onClick={this.toggleMenu}><FontAwesomeIcon icon="bars"/></div>
-				<input className="location-entry" type="text" name="locationField" placeholder="Location"/>
+				<input class="location-entry" type="text" name="locationField" placeholder="Location"/>
 			</div>
-			<MapCont style={{zIndex: '1'}}/>
+			<Map/>
 			</>
 		)
 	}
